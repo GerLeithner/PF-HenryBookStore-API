19eeb7dcd6dc2b7412c183c97296112627833e30
unknown <ketavara97@gmail.com>
2022-12-05 09:23:27 -0600

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+    // proximamente escribiremos codigo aqui}
+    return numero%2==0 ? numero.toString(2) : numero.toString(16);
     }
+
+function kevin(){
+    return {
+        nombre: "Kevin Tavara",
+        edad: 25,
+        nacionalidad: "Peru"
+    }
+}
