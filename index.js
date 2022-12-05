574bf4f44b91027e667b239f8089af02821f752c
gerleithner <ger.leithner@gmail.com>
2022-12-05 02:06:42 -0300

    finishing pandora function

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
-    }
+function laCajaDePandora(n){
+    return n % 2 === 0 ? n.toString(2) : n.toString(16);
+}
+
+// function decToBin(num) {
+
+//     let bin = "";
+//     while(num >= 1){
+//       bin = (num % 2).toString() + bin; 
+//       num = Math.floor(num / 2);
+//     }
+//     return bin;
+// }
