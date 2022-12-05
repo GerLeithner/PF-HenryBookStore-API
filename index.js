f171d5238ce0998b24a107e32d28dcee3623e67c
andrealopezpalomeque <andrealopezpalomeque@gmail.com>
2022-12-05 12:03:54 -0300

    ejercio1 terminado

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
-    }
+function laCajaDePandora(numero) {
+  // proximamente escribiremos codigo aqui
+  return n % 2 === 0 ? numero.toString(2) : numero.toString(16);
+}
+
+function andrea() {
+  return {
+    nombre: "Andrea",
+    edad: "24",
+    nacionalidad: "Argentina",
+  };
+}
