b6120dff99892b281b1a0ff9cdefd86076f0fb1c
gerleithner <ger.leithner@gmail.com>
2022-12-05 02:32:15 -0300

    finishing pandora and german functions

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function laCajaDePandora(n) {
+    return n % 2 === 0 ? n.toString(2) : n.toString(16);
+}
+
+function german() {
+    return {
+        name: "Germán",
+        age: 29,
+        nationality: "argentine"
     }
+}
