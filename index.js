fd4c40904d3bf04ef5ea7ff885b58580386fd308
Adrianl93 <105607840+Adrianl93@users.noreply.github.com>
2023-01-04 20:35:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const port=process.PORT || 3001;
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
     console.log(`Server raised in port ${port}`);
   });
